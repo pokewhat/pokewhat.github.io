53f768b08906551c6e31c2c82b909d85af76a480
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-05 13:33:13 +1000

    User best experience
    soon we will use this data to recommend the user pages from our site

---
+++ bestExp.js
@@ -0,0 +1,14 @@
+/*
+For the best website experience
+
+*/
+var userExp = {
+    sites: []
+}; // If not in localstorage
+if(localStorage.hasOwnProperty("userExp")) {
+  userExp = JSON.parse(localStorage.getItem("userExp")); // If in localStorage.
+} else {
+  window.alert("It is charmandory you accept that we will collect your history on this site for the best experience.");
+}
+userExp.sites.append(window.location.href);
+localStorage.setItem("userExp", JSON.stringify(userExp)); // Save user experience on our site
