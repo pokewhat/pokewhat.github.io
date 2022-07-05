0aff69e2cb6df2f36aaa2ed494a0611e287fb7dc
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-05 14:18:50 +1000

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ var faviconloader = document.createElement("link");
 faviconloader.setAttribute("rel", "icon");
 faviconloader.setAttribute("href", faviconurl);
 document.querySelectorAll("head")[0].append(faviconloader);
+function getImage(url) {
+  const img = new Image();
+  img.addEventListener("load", function() {
+    return this;
+  });
+  img.src = url;
+}
+var redurl = "https://pokewhat.github.io/pokerby/static/red.png";
+var redimage = getImage(redurl);
+var redbluelogo = document.createElement("div");
+redbluelogo.setAttribute("style", "width: " + redimage.naturalWidth + "px; height: " + redimage.naturalHeight + "px; background-image: url(\"" + redurl + "\");");
+document.querySelectorAll("body")[0].append(redbluelogo);
