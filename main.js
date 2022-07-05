e235122ad94891dd60adb7c4b69b0823cdc37167
Buggem <66670928+Buggem@users.noreply.github.com>
2022-07-05 14:27:34 +1000

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ function getImage(url) {
   return img;
 }
 var redurl = "https://pokewhat.github.io/pokerby/static/red.png";
-var redimage = getImage(redurl);
-var redbluelogo = document.createElement("div");
-redbluelogo.setAttribute("style", "width: " + redimage.naturalWidth + "px; height: " + redimage.naturalHeight + "px; background-image: url(\"" + redurl + "\");");
-document.querySelectorAll("body")[0].append(redbluelogo);
+var redimage = new Image();
+redimage.addEventListener("load", function() {
+  var redbluelogo = document.createElement("div");
+  redbluelogo.setAttribute("style", "width: " + redimage.naturalWidth + "px; height: " + redimage.naturalHeight + "px; background-image: url(\"" + redurl + "\");");
+  document.querySelectorAll("body")[0].append(redbluelogo);
+});
+redimage.src = redurl;
